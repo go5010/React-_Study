d800ac33e94edf0c0d2ae764c645b9fbf3f8f699
go5010 <gota.4305@gmail.com>
2023-12-09 17:19:05 +0900

    part28完了

--- React_sec4.js
+++ React_sec4.js
@@ -70,14 +70,43 @@
 // console.log(name);
 
 // オブジェクトの省略記法
-const name="jake";
-const age=31;
+// const name="jake";
+// const age=31;
 
-// const myprofile={
-//   name:name,
-//   age:age,
-// }
+// // const myprofile={
+// //   name:name,
+// //   age:age,
+// // }
+// // console.log(myprofile);
+
+// const myprofile={name, age}
 // console.log(myprofile);
 
-const myprofile={name, age}
-console.log(myprofile);
+// スプレッド構文 ...
+
+// 配列の展開
+// const arr1=[1,2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumfunc = (num1, num2) => console.log(num1 + num2);
+// sumfunc(arr1[0], arr1[1]);
+// sumfunc(...arr1);
+
+// まとめる
+// const arr2=[1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー，結合
+// const arr4=[10,20];
+// const arr5=[30,40];
+
+// const arr6 = [...arr4];
+// // console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
